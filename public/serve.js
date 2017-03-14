915b6103a9106347ec0acfff96458f191b16b038
Katie Zaback <zabackka@miamioh.edu>
2017-03-13 21:38:34 -0400

    updating game files

--- public/serve.js
+++ public/serve.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
