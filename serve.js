9048e5716d7e1007ab7958f790cfd9ccc651f524
Katie Zaback <zabackka@miamioh.edu>
2017-03-13 20:46:38 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -1,14 +1,14 @@
+var http = require('http'),
+    fs = require('fs');
 
-var express = require('express')
-var app = express()
 
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/index.html'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
