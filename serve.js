3cd3976e86ee41244b14479c1ab383e6a501d41e
Katie Zaback <zabackka@miamioh.edu>
2017-03-13 20:52:42 -0400

    changing index location

--- serve.js
+++ serve.js
@@ -3,7 +3,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('./game_index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
