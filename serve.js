49ec1201ecaab7c8c77d7907b5897c643472d2c3
Katie Zaback <zabackka@miamioh.edu>
2017-03-15 21:51:31 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var app = express()
 var path = require('path');
+var cors = require('cors');
+app.use(cors());
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(path.join(__dirname + '/assets'))); 
+
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
