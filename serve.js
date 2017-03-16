0c7752baad0241185877042cf6b7b0485684c35c
Katie Zaback <zabackka@miamioh.edu>
2017-03-15 22:13:32 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -29,7 +29,7 @@ var fs = require('fs');
 
 scriptExecution.stdout.on('data', (data) => {
 	console.log(String.fromCharCode.apply(null, data));
-	fs.writeFile("~/assets/learn.js", "console.log('Hello!')", function(err) {
+	fs.writeFile(__dirname + "/assets/learn.js", "console.log('Hello!')", function(err) {
     if(err) {
         return console.log(err);
     }
