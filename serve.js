f97fcecc59a2c4609967015082d028804a5499a6
Katie Zaback <zabackka@miamioh.edu>
2017-03-15 22:20:15 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -8,16 +8,6 @@ var path = require('path');
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(path.join(__dirname + '/assets'))); 
 
-
-app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/index.html'));
-});
-
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-
 ////////////////////////
 /// ML FUNCTIONALITY ///
 ////////////////////////
@@ -48,5 +38,17 @@ app.post("/getSum", function(request, response) {
 	sum = request.body.sum;
 	return response.json({}, 200);
 });
+
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
 
 
