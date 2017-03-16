da3dc2b2d14443ba8ccee8dff3e3edfa081ff8d3
Katie Zaback <zabackka@miamioh.edu>
2017-03-15 22:43:38 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -8,42 +8,9 @@ var path = require('path');
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(path.join(__dirname + '/assets'))); 
 
-////////////////////////
-/// ML FUNCTIONALITY ///
-////////////////////////
-
-const spawn = require('child_process').spawn;
-const scriptExecution = spawn("python", ["adaptNN.py"]);
-
-var fs = require('fs');
-
-scriptExecution.stdout.on('data', (data) => {
-	console.log(String.fromCharCode.apply(null, data));
-	fs.writeFile(__dirname + "/assets/learn.js", "testFunction();", function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-	}); 
-});
-
-var data = JSON.stringify([1,2,3,4]);
-scriptExecution.stdin.write(data);
-
-scriptExecution.stdin.end();
-
-app.post("/getSum", function(request, response) {
-	var sum; 
-	sum = request.body.sum;
-	return response.json({}, 200);
-});
-
-
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
 });
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
