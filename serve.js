fe8782ba51e06196d5cca9059017875f381a1bce
Katie Zaback <zabackka@miamioh.edu>
2017-03-15 23:11:45 -0400

    updating serve.js

--- serve.js
+++ serve.js
@@ -18,9 +18,9 @@ io = io.listen(server);
 // app.set('port', (process.env.PORT || 8080))
 // app.use(express.static(path.join(__dirname + '/assets'))); 
 
-// app.get('/', function(request, response) {
-// 	response.sendFile(path.join(__dirname + '/index.html'));
-// });
+app.get('/', function(request, response) {
+ 	response.sendFile(path.join(__dirname + '/index.html'));
+ });
 
 // app.listen(app.get('port'), function() {
 //   console.log("Node app is running at localhost:" + app.get('port'));
