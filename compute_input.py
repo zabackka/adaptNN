8b3aa686fa5944dc8de2714e2932efdf13002eaa
Katie Zaback <zabackka@miamioh.edu>
2017-03-20 17:19:01 -0400

    modifying tester files

--- compute_input.py
+++ compute_input.py
@@ -18,6 +18,11 @@ def main():
     #     print (x)
     #     sys.stdout.flush()
     sys.stdout.write(str(57) + '\n')
+    sys.stdout.write(str(57) + '\n')
+    sys.stdout.write(str(57) + '\n')
+    sys.stdout.write(str(57) + '\n')
+    sys.stdout.write(str(57) + '\n')
+    sys.stdout.write(str(57) + '\n')
 
 
 
