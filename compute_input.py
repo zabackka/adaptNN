57a4a515aa3d606eb3eee6f7d173b1ccf04495d1
Katie Zaback <zabackka@miamioh.edu>
2017-03-21 19:10:30 -0400

    modifying tester files

--- compute_input.py
+++ compute_input.py
@@ -22,7 +22,7 @@ def main():
         sys.stdout.flush()
         num_params = num_params + 1
         
-        lines = sys.stdin.readLines()
+        lines = sys.stdin.buffer.read()
         sys.stdout.write(str(lines) + '\n')
         sys.stdout.flush()
 
