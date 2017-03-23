8e3406b1dd43c97b4c173a9f3bd4daf682d7991e
Katie Zaback <zabackka@miamioh.edu>
2017-03-23 00:26:09 -0400

    modifying tester files

--- compute_input.py
+++ compute_input.py
@@ -6,10 +6,11 @@ import random
 def main():
     num_params = int(sys.argv[1])
     
-    msg = sys.stdin.readline()
-    sys.stdout.write(str(msg) + "\n")
-    # sys.stdout.write(str(random.random()) + "\n")
-    sys.stdout.flush()
+    while (true):
+        msg = sys.stdin.readline()
+        sys.stdout.write(str(msg) + "\n")
+        # sys.stdout.write(str(random.random()) + "\n")
+        sys.stdout.flush()
 
 
 #start process
