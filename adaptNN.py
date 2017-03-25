b7fa881efd06c58d69f3a81d1c5cf61f51cdaef7
Katie Zaback <zabackka@miamioh.edu>
2017-03-25 15:01:41 -0400

    modifying tester files

--- adaptNN.py
+++ adaptNN.py
@@ -63,7 +63,7 @@ def main():
 			sys.stderr.flush()
 			
 
-			sys.stdout.write(json.JSONEncoder().encode(testVar) + "\n")
+			sys.stdout.write(json.dumps(testVar) + "\n")
 			sys.stdout.flush()
 
 
