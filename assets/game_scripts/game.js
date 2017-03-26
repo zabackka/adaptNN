b34cf27cf13cea7eca72a7626ac4f7b9baf685e8
Katie Zaback <zabackka@miamioh.edu>
2017-03-25 21:42:33 -0400

    updating game.js to track user performance

--- assets/game_scripts/game.js
+++ assets/game_scripts/game.js
@@ -17,7 +17,7 @@ var REFRESH_RATE = 10;
 var CURRENT_TIME = 0;
 
 var numCollisions = 0; 
-var PLAYER_PERFORMANCE = ((numCollisions / 3000) * 100) - 100;
+var PLAYER_PERFORMANCE = (numCollisions - 3000) / 3000;
 
 var GAME_TIMER = setInterval(updateTime, 1000);
 var LEARNING_LOOP = setInterval(updateParams, 3000);
