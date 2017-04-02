67e946b0be0cd47f4dd713f0607511329d4ff9c3
Katie Zaback <zabackka@miamioh.edu>
2017-04-02 18:26:08 -0400

    working on keeping params within constraint values

--- adaptNN.py
+++ adaptNN.py
@@ -85,7 +85,7 @@ def main():
 				sendBack.append(x)
 
 			# param_cost = param_cost[0]
-			sendBack.append(type(param_cost))
+			sendBack.append(param_cost[0])
 			
 			## RESPOND TO SERVER WITH NEW DATA ##
 			sys.stderr.flush()
