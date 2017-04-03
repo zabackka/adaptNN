2bd5735377cbc7bd8b048ae5d9527d005e654115
Katie Zaback <zabackka@miamioh.edu>
2017-04-03 02:01:52 -0400

    working on keeping params within constraint values

--- adaptNN.py
+++ adaptNN.py
@@ -171,7 +171,8 @@ class FullyConnectedLayer(object):
 	def input_cost(self, net, l_cons, u_cons, train_x):
 		for i in range(0, self.n_input):
 			if T.le(train_x.T[i], l_cons[i]) or T.gt(train_x.T[i], u_cons[i]):
-				sys.stderr.write("OUT OF BOUNDS \n")
+				sys.stderr.write("OUT OF BOUNDS")
+				sys.stderr.write(str(T.pow(T.mean(T.pow((self.output - net.performance_goal), 2)), 10)))
 				return T.pow(T.mean(T.pow((self.output - net.performance_goal), 2)), 10)
 				# return T.mean((self.output - net.performance_goal)) * 100
 
