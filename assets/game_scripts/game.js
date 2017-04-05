57dea2af00cc54e972c9217913350c0a19b5df87
Katie Zaback <zabackka@miamioh.edu>
2017-04-04 21:29:57 -0400

    working on environment update functionality

--- assets/game_scripts/game.js
+++ assets/game_scripts/game.js
@@ -100,11 +100,8 @@ function updateParams() {
 		NNprediction = data[0];
 		// console.log("enemy speed: " + intervalMap(data[1], -Number.MAX_VALUE, Number.MAX_VALUE, 2, 10));
 		// console.log("player speed: " + intervalMap(data[2], -Number.MAX_VALUE, Number.MAX_VALUE, 2, 10));
-		enemySpeed = intervalMap(data[1], -Number.MAX_VALUE, Number.MAX_VALUE, 2.0, 10.0);
-		console.log("interval map: " + intervalMap(data[1], -1, 1, 2.0, 10.0));
-		console.log("enemy speed: " + enemySpeed);
-		playerSpeed = intervalMap(data[2], -Number.MAX_VALUE, Number.MAX_VALUE, 2.0, 10.0);
-		// console.log("player speed: " + playerSpeed + "  |  " + data[2]);
+		enemySpeed = intervalMap(data[1], -1, 1, 2.0, 10.0);
+		playerSpeed = intervalMap(data[2], -1, 1, 2.0, 10.0);
 		paramCost = data[3];
 	});
 	
