b4c12d4ee6caa224d9ddffaa628f3694fc86e986
Katie Zaback <zabackka@miamioh.edu>
2017-05-02 14:56:58 -0400

    testing functionality

--- start.js
+++ start.js
@@ -1,7 +1,5 @@
 //start.js
 
-
-try {
 // initial setup for serving webpage to browser
 var express = require('express')
 var app = express()
@@ -37,65 +35,65 @@ var clients = [];
 io.sockets.on("connection", function(socket) {
 	socket.setMaxListeners(Infinity);
 
-	// assign CLIENT ID
-	var clientID = NUM_CLIENTS;
+	// // assign CLIENT ID
+	// var clientID = NUM_CLIENTS;
 	
-	// add new client to list of clients
-	clients.push(socket);
-	NUM_CLIENTS++;
+	// // add new client to list of clients
+	// clients.push(socket);
+	// NUM_CLIENTS++;
 	
-	// log new client connection to terminal
-	console.log("CONNECTED CLIENT [" + clientID + "]");
+	// // log new client connection to terminal
+	// console.log("CONNECTED CLIENT [" + clientID + "]");
 
-	// spawn child process (python script)
-	const err = fs.openSync('err.txt', 'a');
-	var sp = require('child_process').spawn;
-	var py = sp('python', ['adaptNN.py', NUM_PARAMS], {
-		stdio: ['pipe', 'pipe', err]
-	});
+	// // spawn child process (python script)
+	// const err = fs.openSync('err.txt', 'a');
+	// var sp = require('child_process').spawn;
+	// var py = sp('python', ['adaptNN.py', NUM_PARAMS], {
+	// 	stdio: ['pipe', 'pipe', err]
+	// });
 
-	py.stdin.setMaxListeners(Infinity);
+	// py.stdin.setMaxListeners(Infinity);
  
-	var writeStream = fs.createWriteStream(SESSION_ID + "-" + clientID + ".csv", {flags: 'a'});
+	// var writeStream = fs.createWriteStream(SESSION_ID + "-" + clientID + ".csv", {flags: 'a'});
 
-	// triggered when client sends a message
-	socket.on("message", function(data) {
-		// parse message & display to console
-		data = JSON.parse(data);	
+	// // triggered when client sends a message
+	// socket.on("message", function(data) {
+	// 	// parse message & display to console
+	// 	data = JSON.parse(data);	
 		
-		var date = new Date();
-		writeStream.write(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds() + "," + data[0] + "," + data[1] + "\n");
-		//console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds() + "\n");
-		// retrieve params & performance
+	// 	var date = new Date();
+	// 	writeStream.write(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds() + "," + data[0] + "," + data[1] + "\n");
+	// 	//console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds() + "\n");
+	// 	// retrieve params & performance
 
-		/** PRINT check statements */
-		// var params = data[0];
-		// var performance = data[1];
-		// console.log("params: " + params);
-		// console.log("performance: " + performance);
+	// 	/** PRINT check statements */
+	// 	// var params = data[0];
+	// 	// var performance = data[1];
+	// 	// console.log("params: " + params);
+	// 	// console.log("performance: " + performance);
 
-		// send data from client to python child process
-		py.stdin.write(JSON.stringify(data) + "\n");
+	// 	// send data from client to python child process
+	// 	py.stdin.write(JSON.stringify(data) + "\n");
 		
-		// receive output from python child process
-		py.stdout.once('data', (data) => {
+	// 	// receive output from python child process
+	// 	py.stdout.once('data', (data) => {
 			
-			/** PRINT check statement **/
-			//console.log("-->received from server: " + data);
+	// 		/** PRINT check statement **/
+	// 		//console.log("-->received from server: " + data);
 			
-			// parse data
-			data = JSON.parse(data);
-			// send modified input data back to client
-			clients[clientID].emit('data', data);
-		});
+	// 		// parse data
+	// 		data = JSON.parse(data);
+	// 		// send modified input data back to client
+	// 		clients[clientID].emit('data', data);
+	// 	});
 
-		// handle end of python child process
-		// should only be triggered in error 
-		// --> [check 'err.txt' for details]
-		py.stdout.on('end', () => {
-			console.log("end!");
-		});
-	});
+	// 	// handle end of python child process
+	// 	// should only be triggered in error 
+	// 	// --> [check 'err.txt' for details]
+	// 	py.stdout.on('end', () => {
+	// 		console.log("end!");
+	// 	});
+	// });
 
 	// handle CLIENT disconnect
 	socket.on('disconnect', function() {
@@ -105,9 +103,7 @@ io.sockets.on("connection", function(socket) {
 	});
 });
 
-} catch (e) {
-	console.log(e);
-}
+
 
 
 
