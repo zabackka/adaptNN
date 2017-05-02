787bad784d6e520ca66654c4957156929bf5d7ad
Katie Zaback <zabackka@miamioh.edu>
2017-05-02 17:23:57 -0400

    testing functionality

--- adaptNN.py
+++ adaptNN.py
@@ -211,7 +211,7 @@ class Network(object):
 		input_cost = self.layers[-1].input_cost(self)
 
 		input_gradients = T.grad(input_cost, self.x)
-		environment_updates = [(train_x, learning_rate2*train_x-input_gradients)]
+		environment_updates = [(train_x, train_x-learning_rate2*input_gradients)]
 
 		# holds a dummy variable for input
 		i = T.lscalar()
