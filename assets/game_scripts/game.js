9399984e613695453f695eecb75f37c9da9418f0
Katie Zaback <zabackka@miamioh.edu>
2017-05-03 15:24:52 -0400

    testing functionality

--- assets/game_scripts/game.js
+++ assets/game_scripts/game.js
@@ -125,7 +125,8 @@ function updateParams() {
 		// playerHeight = intervalMap(data[5] * 1000000000000, 0, 1000000000000, 80.0, 150.0);
 		// playerWidth = intervalMap(data[6] * 1000000000000, 0, 1000000000000, 40.0, 80.0);
 		// playerSpeed = intervalMap(data[7] * 1000000000000, 0, 1000000000000, 10.0, 20.0);
-
+		console.log("raw data: " + data[1]);
+		console.log("modified data: " + intervalMap(data[1] * 1000000000000, 0, 1000000000000, 1000.0, 5000.0));
 		// enemyHeight = data[1] * 10;
 		// enemyWidth = data[2] * 40; 
 		// enemySpeed = data[3] * 2; 
