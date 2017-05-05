62f7abf8f7bcde30a16e733b630bb5e5551e98b5
Katie Zaback <zabackka@miamioh.edu>
2017-05-05 00:22:47 -0400

    testing functionality

--- adaptNN.py
+++ adaptNN.py
@@ -60,7 +60,7 @@ def main():
 
 			# create new storage vars for training data
 			train_datax = numpy.empty((batch_size, num_params))
-			train_datay = numpy.empty(batch_size,)
+			train_datay = numpy.empty(,batch_size)
 			for x in range(0, batch_size):
 				train_datay[x] = performance
 			
