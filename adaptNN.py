2aaf7d4c465242e75b6e8673763381d860678d03
Katie Zaback <zabackka@miamioh.edu>
2017-05-08 21:23:19 -0400

    testing

--- adaptNN.py
+++ adaptNN.py
@@ -192,7 +192,7 @@ class FullyConnectedLayer(object):
 
 	# define the cost of these input (environment) params
 	def input_cost(self, net, output):
-		return T.abs_(T.mean((net.performance_goal - output)))
+		return T.abs_(T.mean((net.performance_goal - 50.0)))
 
 	# define the cost of this layer
 	def network_cost(self, net):
